acb41f9b183bf0d9670cc782c4197ef444439351
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2022-04-13 08:18:26 +0200

    Move all changes to vina_dock.py from vina_docking.py and removed the latter from repo

--- moldock/vina_dock.py
+++ moldock/vina_dock.py
@@ -4,9 +4,7 @@
 
 import argparse
 import os
-import re
 import sqlite3
-import subprocess
 import sys
 import tempfile
 from functools import partial
@@ -15,155 +13,13 @@ from multiprocessing import Pool, Manager, cpu_count
 import dask
 from dask import bag
 from dask.distributed import Lock as daskLock, Client
-from meeko import MoleculePreparation
-from meeko import obutils
-from openbabel import openbabel as ob
-from rdkit import Chem
-from rdkit.Chem import AllChem
-from moldock.read_input import read_input
 from vina import Vina
+from moldock.preparation_for_docking import create_db, save_sdf, add_protonation, ligand_preparation, \
+    fix_pdbqt, pdbqt2molblock, cpu_type, filepath_type
 
 
 class RawTextArgumentDefaultsHelpFormatter(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
     pass
-
-
-def cpu_type(x):
-    return max(1, min(int(x), cpu_count()))
-
-
-def filepath_type(x):
-    if x:
-        return os.path.abspath(x)
-    else:
-        return x
-
-
-def add_protonation(db_fname):
-    '''
-    Protonate SMILES by Chemaxon cxcalc utility to get molecule ionization states at pH 7.4.
-    Parse console output and update db.
-    :param conn:
-    :return:
-    '''
-    conn = sqlite3.connect(db_fname)
-
-    try:
-        cur = conn.cursor()
-        protonate, done = list(cur.execute('SELECT protonation, protonation_done FROM setup'))[0]
-
-        if protonate and not done:
-            smiles_dict = cur.execute('SELECT smi, id FROM mols')
-            if not smiles_dict:
-                sys.stderr.write(f'no molecules to protonate')
-                return
-
-            smiles, mol_ids = zip(*smiles_dict)
-
-            with tempfile.NamedTemporaryFile(suffix='.smi', mode='w', encoding='utf-8') as tmp:
-                tmp.writelines(['\n'.join(smiles)])
-                tmp.seek(0)
-                cmd_run = f"cxcalc majormicrospecies -H 7.4 -f smiles -M -K '{tmp.name}'"
-                smiles_protonated = subprocess.check_output(cmd_run, shell=True).decode().split()
-
-            for mol_id, smi_protonated in zip(mol_ids, smiles_protonated):
-                cur.execute("""UPDATE mols
-                               SET smi_protonated = ?
-                               WHERE
-                                   id = ?
-                            """, (Chem.MolToSmiles(Chem.MolFromSmiles(smi_protonated), isomericSmiles=True), mol_id))
-            conn.commit()
-            cur.execute('UPDATE setup SET protonation_done = 1')
-            conn.commit()
-
-    finally:
-        conn.close()
-
-
-def mk_prepare_ligand_string(molecule_string, build_macrocycle=True, add_water=False, merge_hydrogen=True,
-                             add_hydrogen=False, pH_value=None, verbose=False, mol_format='SDF'):
-
-    mol = obutils.load_molecule_from_string(molecule_string, molecule_format=mol_format)
-
-    if pH_value is not None:
-        mol.CorrectForPH(float(pH_value))
-
-    if add_hydrogen:
-        mol.AddHydrogens()
-        charge_model = ob.OBChargeModel.FindType("Gasteiger")
-        charge_model.ComputeCharges(mol)
-
-    m = Chem.MolFromMolBlock(molecule_string)
-    amide_rigid = len(m.GetSubstructMatch(Chem.MolFromSmarts('[C;!R](=O)[#7]([!#1])[!#1]'))) == 0
-
-    preparator = MoleculePreparation(merge_hydrogens=merge_hydrogen, macrocycle=build_macrocycle,
-                                     hydrate=add_water, amide_rigid=amide_rigid)
-                                     #additional parametrs
-                                     #rigidify_bonds_smarts=[], rigidify_bonds_indices=[])
-    preparator.prepare(mol)
-    if verbose:
-        preparator.show_setup()
-
-    return preparator.write_pdbqt_string()
-
-
-def ligand_preparation(smi, seed=0):
-
-    def convert2mol(m):
-
-        def gen_conf(mol, useRandomCoords, randomSeed):
-            params = AllChem.ETKDGv3()
-            params.useRandomCoords = useRandomCoords
-            params.randomSeed = randomSeed
-            conf_stat = AllChem.EmbedMolecule(mol, params)
-            return mol, conf_stat
-
-        if not m:
-            return None
-        m = Chem.AddHs(m, addCoords=True)
-        m, conf_stat = gen_conf(m, useRandomCoords=False, randomSeed=seed)
-        if conf_stat == -1:
-            # if molecule is big enough and rdkit cannot generate a conformation - use params.useRandomCoords = True
-            m, conf_stat = gen_conf(m, useRandomCoords=True, randomSeed=seed)
-            if conf_stat == -1:
-                return None
-        AllChem.UFFOptimizeMolecule(m, maxIters=100)
-        return Chem.MolToMolBlock(m)
-
-    mol = Chem.MolFromSmiles(smi)
-    mol_conf_sdf = convert2mol(mol)
-    if mol_conf_sdf is None:
-        return None
-    mol_conf_pdbqt = mk_prepare_ligand_string(mol_conf_sdf,
-                                              build_macrocycle=False,
-                                              # can do it True, but there is some problem with >=7-chains mols
-                                              add_water=False, merge_hydrogen=True, add_hydrogen=False,
-                                              # pH_value=7.4, can use this opt but some results are different in comparison to chemaxon
-                                              verbose=False, mol_format='SDF')
-    return mol_conf_pdbqt
-
-
-def fix_pdbqt(pdbqt_block):
-    pdbqt_fixed = []
-    for line in pdbqt_block.split('\n'):
-        if not line.startswith('HETATM') and not line.startswith('ATOM'):
-            pdbqt_fixed.append(line)
-            continue
-        atom_type = line[12:16].strip()
-        # autodock vina types
-        if 'CA' in line[77:79]: #Calcium is exception
-            atom_pdbqt_type = 'CA'
-        else:
-            atom_pdbqt_type = re.sub('D|A', '', line[77:79]).strip() # can add meeko macrocycle types (G and \d (CG0 etc) in the sub expression if will be going to use it
-
-        if re.search('\d', atom_type[0]) or len(atom_pdbqt_type) == 2: #1HG or two-letter atom names such as CL,FE starts with 13
-            atom_format_type = '{:<4s}'.format(atom_type)
-        else: # starts with 14
-            atom_format_type = ' {:<3s}'.format(atom_type)
-        line = line[:12] + atom_format_type + line[16:]
-        pdbqt_fixed.append(line)
-
-    return '\n'.join(pdbqt_fixed)
 
 
 def docking(ligands_pdbqt_string, receptor_pdbqt_fname, center, box_size, exhaustiveness, seed, n_poses, ncpu):
@@ -185,33 +41,12 @@ def docking(ligands_pdbqt_string, receptor_pdbqt_fname, center, box_size, exhaus
     return v.energies(n_poses=1)[0][0], v.poses(n_poses=1)
 
 
-def pdbqt2molblock(pdbqt_block, smi, mol_id):
-    mol_block = None
-    mol = Chem.MolFromPDBBlock('\n'.join([i[:66] for i in pdbqt_block.split('MODEL')[1].split('\n')]), removeHs=False, sanitize=False)
-    if mol:
-        try:
-            template_mol = Chem.MolFromSmiles(smi)
-            # explicit hydrogends are removed from carbon atoms (chiral hydrogens) to match pdbqt mol,
-            # e.g. [NH3+][C@H](C)C(=O)[O-]
-            template_mol = Chem.AddHs(template_mol, explicitOnly=True,
-                                      onlyOnAtoms=[a.GetIdx() for a in template_mol.GetAtoms() if
-                                                   a.GetAtomicNum() != 6])
-            mol = AllChem.AssignBondOrdersFromTemplate(template_mol, mol)
-            Chem.SanitizeMol(mol)
-            Chem.AssignStereochemistry(mol, cleanIt=True, force=True, flagPossibleStereoCenters=True)
-            mol.SetProp('_Name', mol_id)
-            mol_block = Chem.MolToMolBlock(mol)
-        except Exception:
-            sys.stderr.write(f'Could not assign bond orders while parsing PDB: {mol_id}\n')
-    return mol_block
-
-
 def process_mol_docking(mol_id, smi, receptor_pdbqt_fname, center, box_size, dbname, seed, exhaustiveness, n_poses, ncpu,
-                        lock=None):
+                        table_name, lock=None):
 
-    def insert_data(dbname, pdbqt_out, score, mol_block, mol_id):
+    def insert_data(dbname, pdbqt_out, score, mol_block, mol_id, table_name='mols'):
         with sqlite3.connect(dbname) as conn:
-            conn.execute("""UPDATE mols
+            conn.execute(f"""UPDATE {table_name}
                                SET pdb_block = ?,
                                    docking_score = ?,
                                    mol_block = ?,
@@ -234,15 +69,15 @@ def process_mol_docking(mol_id, smi, receptor_pdbqt_fname, center, box_size, dbn
 
     if lock is not None:  # multiprocessing
         with lock:
-            insert_data(dbname, pdbqt_out, score, mol_block, mol_id)
+            insert_data(dbname, pdbqt_out, score, mol_block, mol_id, table_name)
     else:  # dask
         with daskLock(dbname):
-            insert_data(dbname, pdbqt_out, score, mol_block, mol_id)
+            insert_data(dbname, pdbqt_out, score, mol_block, mol_id, table_name)
 
     return mol_id
 
 
-def iter_docking(dbname, receptor_pdbqt_fname, protein_setup, protonation, exhaustiveness, seed, n_poses, ncpu, use_dask,
+def iter_docking(dbname, table_name, receptor_pdbqt_fname, protein_setup, protonation, exhaustiveness, seed, n_poses, ncpu, use_dask,
                  add_sql=None):
     '''
     This function should update output db with docked poses and scores. Docked poses should be stored as pdbqt (source)
@@ -274,10 +109,11 @@ def iter_docking(dbname, receptor_pdbqt_fname, protein_setup, protonation, exhau
                            (config['size_x'], config['size_y'], config['size_z'])
         return center, box_size
 
+
     with sqlite3.connect(dbname) as conn:
         cur = conn.cursor()
         smi_field_name = 'smi_protonated' if protonation else 'smi'
-        sql = f"SELECT id, {smi_field_name} FROM mols WHERE docking_score IS NULL"
+        sql = f"SELECT id, {smi_field_name} FROM {table_name} WHERE docking_score IS NULL AND {smi_field_name} != ''"
         if isinstance(add_sql, str) and add_sql:
             sql += f" AND {add_sql}"
         smiles_dict = dict(cur.execute(sql))
@@ -292,7 +128,7 @@ def iter_docking(dbname, receptor_pdbqt_fname, protein_setup, protonation, exhau
         for i, mol_id in enumerate(b.starmap(process_mol_docking,
                                              receptor_pdbqt_fname=receptor_pdbqt_fname, center=center,
                                              box_size=box_size, dbname=dbname, exhaustiveness=exhaustiveness,
-                                             seed=seed, n_poses=n_poses, ncpu=1).compute(),
+                                             seed=seed, n_poses=n_poses, table_name=table_name, ncpu=1).compute(),
                                    1):
             if i % 100 == 0:
                 sys.stderr.write(f'\r{i} molecules were docked')
@@ -304,65 +140,13 @@ def iter_docking(dbname, receptor_pdbqt_fname, protein_setup, protonation, exhau
         lock = manager.Lock()
         i = 0
         for i, mol_id in enumerate(pool.starmap(partial(process_mol_docking, dbname=dbname,
-                                                        receptor_pdbqt_fname=receptor_pdbqt_fname,
-                                                        center=center, box_size=box_size, seed=seed,
-                                                        exhaustiveness=exhaustiveness, n_poses=n_poses, ncpu=1, lock=lock),
+                                                        receptor_pdbqt_fname=receptor_pdbqt_fname, center=center,
+                                                        box_size=box_size, seed=seed, exhaustiveness=exhaustiveness,
+                                                        table_name=table_name, n_poses=n_poses, ncpu=1, lock=lock),
                                                 smiles_dict.items()), 1):
             if i % 100 == 0:
                 sys.stderr.write(f'\r{i} molecules were docked')
         sys.stderr.write(f'\r{i} molecules were docked\n')
-
-
-def create_db(db_fname, input_fname, protonation, pdbqt_fname, protein_setup_fname, prefix):
-    conn = sqlite3.connect(db_fname)
-    cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS mols
-            (
-             id TEXT PRIMARY KEY,
-             smi TEXT,
-             smi_protonated TEXT,
-             docking_score REAL,
-             pdb_block TEXT,
-             mol_block TEXT,
-             time TEXT
-            )""")
-    conn.commit()
-    data = [(f'{prefix}-{mol_name}' if prefix else mol_name,
-             Chem.MolToSmiles(mol, isomericSmiles=True))
-            for mol, mol_name in read_input(input_fname)]
-    cur.executemany(f'INSERT INTO mols (id, smi) VALUES(?, ?)', data)
-    conn.commit()
-
-    cur.execute("""CREATE TABLE IF NOT EXISTS setup
-            (
-             protonation INTEGER,
-             protonation_done INTEGER DEFAULT 0,
-             protein_pdbqt TEXT,
-             protein_setup TEXT
-            )""")
-    conn.commit()
-    pdbqt_string = open(pdbqt_fname).read()
-    setup_string = open(protein_setup_fname).read()
-    cur.execute('INSERT INTO setup VALUES (?,?,?,?)', (int(protonation), 0, pdbqt_string, setup_string))
-    conn.commit()
-
-    conn.close()
-
-
-def save_sdf(db_fname):
-    sdf_fname = os.path.splitext(db_fname)[0] + '.sdf'
-    with open(sdf_fname, 'wt') as w:
-        conn = sqlite3.connect(db_fname)
-        cur = conn.cursor()
-        for mol_block, mol_name, score in cur.execute('SELECT mol_block, id, docking_score '
-                                                      'FROM mols '
-                                                      'WHERE docking_score IS NOT NULL '
-                                                      'AND mol_block IS NOT NULL'):
-            w.write(mol_block + '\n')
-            w.write(f'>  <ID>\n{mol_name}\n\n')
-            w.write(f'>  <docking_score>\n{score}\n\n')
-            w.write('$$$$\n')
-        sys.stderr.write(f'Best poses were saved to {sdf_fname}\n')
 
 
 def main():
@@ -425,6 +209,8 @@ def main():
                         help='number of cpus.')
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
                         help='print progress to STDERR.')
+    parser.add_argument('--table_name', metavar='STRING', required=False, default='mols', choices=['mols', 'tautomers'],
+                        help='name of table in database.')
 
     args = parser.parse_args()
 
@@ -449,7 +235,9 @@ def main():
     setup.flush()
     protonation = list(conn.execute('SELECT protonation FROM setup'))[0][0]
 
-    iter_docking(dbname=args.output, receptor_pdbqt_fname=protein.name, protein_setup=setup.name,
+    # table_name = 'mols'
+
+    iter_docking(dbname=args.output, table_name=args.table_name, receptor_pdbqt_fname=protein.name, protein_setup=setup.name,
                  protonation=protonation, exhaustiveness=args.exhaustiveness, seed=args.seed, n_poses=args.n_poses, ncpu=args.ncpu,
                  use_dask=args.hostfile is not None)
 
