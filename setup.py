5f063e6423858dd7e44537b54e56ca384f65b5b3
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2022-10-19 00:38:01 +0200

    Add get_sdf_from_dock_db.py and change setup.py to call the new script from command line

--- setup.py
+++ setup.py
@@ -28,5 +28,6 @@ setuptools.setup(
     },
     entry_points={'console_scripts':
                       ['vina_dock = moldock.vina_dock:main',
-                       'gnina_dock = moldock.gnina_dock:main']}
+                       'gnina_dock = moldock.gnina_dock:main',
+                       'get_sdf_from_dock_db = moldock.get_sdf_from_dock_db:main']}
 )
