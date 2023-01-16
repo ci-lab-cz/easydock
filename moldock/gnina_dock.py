2dddfd2a29c09a8f3f33d9545f29af6095f4a947
GuzelMinibaeva <44097050+GuzelMinibaeva@users.noreply.github.com>
2023-01-16 10:42:41 +0100

    Fix extracting score from pdbqt file for vinardo (#10)

--- moldock/gnina_dock.py
+++ moldock/gnina_dock.py
@@ -43,7 +43,11 @@ def docking(script_file, ligand_pdbqt_file, ligand_out_fname, receptor_pdbqt_fna
 
 def get_pdbqt_and_score(ligand_out_fname):
     pdbqt_out = open(ligand_out_fname).read()
-    score = round(float(pdbqt_out.split(' ')[7].split('REMARK')[0]), 3) #get CNNaffinity
+    string_with_score = pdbqt_out.split('MODEL')[1].split('\n')[1].split(' ')
+    if 'CNNaffinity' in string_with_score:
+        score = round(float(string_with_score.split('CNNaffinity ')[1].split('REMARK')[0])) #get CNNaffinity
+    else:
+        score = round(float(string_with_score.split('minimizedAffinity ')[1].split('REMARK')[0]))
     return score, pdbqt_out
 
 
