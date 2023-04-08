94ba2bc43e9f29ba8c0e2373622639e99100a3bc
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2023-04-08 22:37:24 +0200

    Create run_dock.py, revised vina_dock and preparation_for_dockign, dask support not implemented

--- moldock/preparation_for_docking.py
+++ moldock/preparation_for_docking.py
@@ -1,4 +1,3 @@
-import json
 import os
 import re
 import sqlite3
@@ -6,14 +5,14 @@ import subprocess
 import sys
 import tempfile
 import traceback
-from multiprocessing import Pool, Manager, cpu_count
+import yaml
+from multiprocessing import cpu_count
 from copy import deepcopy
 
 from meeko import MoleculePreparation
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from moldock import read_input
-# from read_input import read_input
 
 
 def cpu_type(x):
@@ -40,81 +39,84 @@ def mol_from_smi_or_molblock(ligand_string):
     return mol
 
 
-def add_protonation(db_fname):
+def add_protonation(db_fname, table_name='mols', add_sql=''):
     '''
     Protonate SMILES by Chemaxon cxcalc utility to get molecule ionization states at pH 7.4.
     Parse console output and update db.
     :param db_fname:
+    :param table_name: table name with molecules to protonate
+    :param add_sql: additional SQL query to be appended to the SQL query to retrieve molecules for protonation,
+                    e.g. "AND iteration=MAX(iteration)".
     :return:
     '''
     conn = sqlite3.connect(db_fname)
 
     try:
         cur = conn.cursor()
-        protonate, done = list(cur.execute('SELECT protonation, protonation_done FROM setup'))[0]
 
-        if protonate and not done:
-            data_list = list(cur.execute('SELECT smi, source_mol_block, id FROM mols'))
-            if not data_list:
-                sys.stderr.write(f'no molecules to protonate')
-                return
+        sql = f"""SELECT smi, source_mol_block, id 
+                  FROM {table_name} 
+                  WHERE docking_score is NULL AND smi_protonated is NULL """
+        sql += add_sql
 
-            smi_ids = []
-            mol_ids = []
-            for i, (smi, mol_block, mol_name) in enumerate(data_list):
-                if mol_block is None:
-                    smi_ids.append(mol_name)
-                    # add missing mol blocks
-                    m = Chem.MolFromSmiles(data_list[i][0])
-                    m.SetProp('_Name', mol_name)
-                    m_block = Chem.MolToMolBlock(m)
-                    data_list[i] = (data_list[i][0],) + (m_block,) + (data_list[i][2],)
-                else:
-                    mol_ids.append(mol_name)
-            smi_ids = set(smi_ids)
-            mol_ids = set(mol_ids)
+        data_list = list(cur.execute(sql))
+        if not data_list:
+            sys.stderr.write(f'no molecules to protonate')
+            return
 
-            output_data_smi = []
-            output_data_mol = []
-            with tempfile.NamedTemporaryFile(suffix='.sdf', mode='w', encoding='utf-8') as tmp:
-                fd, output = tempfile.mkstemp()  # use output file to avoid overflow of stdout is extreme cases
-                try:
-                    for _, mol_block, _ in data_list:
-                        tmp.write(mol_block)
-                        tmp.write('\n$$$$\n')
-                    tmp.flush()
-                    cmd_run = f"cxcalc -S majormicrospecies -H 7.4 -M -K '{tmp.name}' > '{output}'"
-                    subprocess.call(cmd_run, shell=True)
-                    sdf_protonated = Chem.SDMolSupplier(output)
-                    for mol in sdf_protonated:
-                        mol_name = mol.GetProp('_Name')
-                        smi = mol.GetPropsAsDict().get('MAJORMS', None)
-                        if smi is not None:
-                            cansmi = Chem.CanonSmiles(smi)
-                            if mol_name in smi_ids:
-                                output_data_smi.append((cansmi, mol_name))
-                            elif mol_name in mol_ids:
-                                output_data_mol.append((cansmi, Chem.MolToMolBlock(mol), mol_name))
-                finally:
-                    os.remove(output)
+        smi_ids = []
+        mol_ids = []
+        for i, (smi, mol_block, mol_name) in enumerate(data_list):
+            if mol_block is None:
+                smi_ids.append(mol_name)
+                # add missing mol blocks
+                m = Chem.MolFromSmiles(data_list[i][0])
+                m.SetProp('_Name', mol_name)
+                m_block = Chem.MolToMolBlock(m)
+                data_list[i] = (smi, m_block, mol_name)
+            else:
+                mol_ids.append(mol_name)
+        smi_ids = set(smi_ids)
+        mol_ids = set(mol_ids)
 
-            cur.executemany(f"""UPDATE mols
-                           SET 
-                               smi_protonated = ?
-                           WHERE
-                               id = ?
-                        """, output_data_smi)
-            cur.executemany(f"""UPDATE mols
-                           SET 
-                               smi_protonated = ?, 
-                               source_mol_block_protonated = ?
-                           WHERE
-                               id = ?
-                        """, output_data_mol)
-            conn.commit()
+        output_data_smi = []
+        output_data_mol = []
+        with tempfile.NamedTemporaryFile(suffix='.sdf', mode='w', encoding='utf-8') as tmp:
+            fd, output = tempfile.mkstemp()  # use output file to avoid overflow of stdout in extreme cases
+            try:
+                for _, mol_block, _ in data_list:
+                    tmp.write(mol_block)
+                    tmp.write('\n$$$$\n')
+                tmp.flush()
+                cmd_run = f"cxcalc -S majormicrospecies -H 7.4 -M -K '{tmp.name}' > '{output}'"
+                subprocess.call(cmd_run, shell=True)
+                sdf_protonated = Chem.SDMolSupplier(output)
+                for mol in sdf_protonated:
+                    mol_name = mol.GetProp('_Name')
+                    smi = mol.GetPropsAsDict().get('MAJORMS', None)
+                    if smi is not None:
+                        cansmi = Chem.CanonSmiles(smi)
+                        if mol_name in smi_ids:
+                            output_data_smi.append((cansmi, mol_name))
+                        elif mol_name in mol_ids:
+                            output_data_mol.append((cansmi, Chem.MolToMolBlock(mol), mol_name))
+            finally:
+                os.remove(output)
 
-            cur.execute('UPDATE setup SET protonation_done = 1')
-            conn.commit()
+        cur.executemany(f"""UPDATE {table_name}
+                       SET 
+                           smi_protonated = ?
+                       WHERE
+                           id = ?
+                    """, output_data_smi)
+        cur.executemany(f"""UPDATE {table_name}
+                       SET 
+                           smi_protonated = ?, 
+                           source_mol_block_protonated = ?
+                       WHERE
+                           id = ?
+                    """, output_data_mol)
+        conn.commit()
 
     finally:
         conn.close()
@@ -266,13 +268,16 @@ def pdbqt2molblock(pdbqt_block, template_mol, mol_id):
     return mol_block
 
 
-def create_db(db_fname, args, args_to_fields=('protein', 'protein_setup')):
+def create_db(db_fname, args, args_to_save=(), config_args_to_save=('protein', 'protein_setup')):
     """
-    Create empty database structure and the setup table, which is filled with values
+    Create empty database structure and the setup table, which is filled with values. To setup table two fields are
+    always stored: yaml file with all input args of the docking script and yaml file with docking config
     :param db_fname: file name of output DB
     :param args: argparse namespace
-    :param args_to_fields: list of arg names which values are file names which content should be stored as separate
-                           fields in setup table
+    :param args_to_save: list of arg names which values are file names which content should be stored as separate
+                         fields in setup table
+    :param config_args_to_save: list of arg names from config file which values are file names which content should be
+                                stored as separate fields in setup table
     :return:
     """
     os.makedirs(os.path.dirname(os.path.abspath(db_fname)), exist_ok=True)
@@ -291,26 +296,38 @@ def create_db(db_fname, args, args_to_fields=('protein', 'protein_setup')):
              time TEXT
             )""")
 
-    # this will create a setup table with the first item in JSON format which contains all input args, and additional
-    # fields with names identical to selected arg names pointed out on text files (e.g protein.pdbqt, protein setup
-    # file, etc). These additional fields will store content of those files as TEXT
-    if isinstance(args_to_fields, (list, tuple, set)):
-        args_to_fields = ['json'] + list(args_to_fields)
-    else:
-        args_to_fields = ['json']
-    sql = f"CREATE TABLE IF NOT EXISTS setup ({', '.join(v + ' TEXT' for v in args_to_fields)})"
+    # this will create a setup table with the first item in YAML format which contains all input args, and additional
+    # fields with names identical to selected arg names pointed out on text files (e.g config, protein.pdbqt,
+    # protein setup file, etc). These additional fields will store content of those files as TEXT
+    args_fields = ['yaml', 'config']
+    if isinstance(args_to_save, (list, tuple, set)):
+        args_fields += list(args_to_save)
+    if isinstance(config_args_to_save, (list, tuple, set)):
+        args_fields += list(config_args_to_save)
+    if len(set(args_fields)) < len(args_fields):
+        raise ValueError('Some arguments which will be stored to DB as separate fields with text files content are '
+                         'duplicated. Please fix this.\n')
+
+    sql = f"CREATE TABLE setup ({', '.join(v + ' TEXT' for v in args_fields)})"
     cur.execute(sql)
     conn.commit()
 
     d = deepcopy(args.__dict__)
-    values = []
-    for v in args_to_fields[1:]:
+    values = [yaml.safe_dump(d), open(d['config']).read()]
+
+    for v in args_to_save:
         if d[v] is not None:
             values.append(open(d[v]).read())
-            d[v] = f'<<{os.path.splitext(d[v])[1]}>>'  # this is a special substitution to designate which args should be loaded from the table, the value is the file extention
         else:
             values.append(None)
-    values = [json.dumps(d, sort_keys=True, indent=2)] + values
+
+    config_dict = yaml.safe_load(open(args.config))
+    for v in config_args_to_save:
+        if config_dict[v] is not None:
+            values.append(open(config_dict[v]).read())
+        else:
+            values.append(None)
+
     cur.execute(f"INSERT INTO setup VALUES ({','.join('?' * len(values))})", values)
     conn.commit()
 
@@ -319,7 +336,7 @@ def create_db(db_fname, args, args_to_fields=('protein', 'protein_setup')):
 
 def restore_setup_from_db(db_fname):
     """
-    Reads stored JSON and creates temporary text files from additional fields in the setup table.
+    Reads stored YAML and creates temporary text files from additional fields in the setup table.
     Returns a dictionary of args and values to be assigned to argparse namespace
     :param db_fname: SQLite DB file name
     :return: dictionary of arguments and their values
@@ -329,62 +346,118 @@ def restore_setup_from_db(db_fname):
     res = cur.execute('SELECT * FROM setup')
     colnames = [d[0] for d in res.description]
     values = [v for v in res][0]
-    for colname, value in zip(colnames, values):
-        if colname == 'json':
-            d = json.loads(value)
-        else:
-            tmppath = tempfile.NamedTemporaryFile(suffix=d[colname][2:-2], mode='w', encoding='utf-8')
-            tmppath.write(value)
-            tmppath.flush()
-            d[colname] = tmppath.name
+    values = dict(zip(colnames, values))
     conn.close()
-    return d
+
+    d = yaml.safe_load(values['yaml'])
+    c = yaml.safe_load(values['config'])
+
+    del values['yaml']
+
+    tmpfiles = []
+
+    try:
+
+        for colname, value in values.items():
+            if colname == 'config':
+                continue
+            if colname in list(d.keys()):
+                suffix = '.' + d[colname].rsplit('.', 1)[1]
+                tmppath = tempfile.mkstemp(suffix=suffix, text=True)
+                d[colname] = tmppath[1]
+                tmpfiles.append(tmppath[1])
+            elif colname in c.keys():
+                suffix = '.' + c[colname].rsplit('.', 1)[1]
+                tmppath = tempfile.mkstemp(suffix=suffix, text=True)
+                c[colname] = tmppath[1]
+                tmpfiles.append(tmppath[1])
+            else:
+                raise KeyError(f'During loading no relevant argument name was found to match the loading field name: {colname}')
+            open(tmppath[1], 'wt').write(value)
+
+        tmppath = tempfile.mkstemp(suffix='.yml', text=True)
+        d['config'] = tmppath[1]
+        tmpfiles.append(tmppath[1])
+        open(tmppath[1], 'wt').write(values['config'])
+
+    except Exception as e:
+        for fname in tmpfiles:
+            os.unlink(fname)
+        raise e
+
+    return d, tmpfiles
 
 
-# def create_db(db_fname, input_fname, protonation, pdbqt_fname, protein_setup_fname, prefix):
-#     conn = sqlite3.connect(db_fname)
-#     cur = conn.cursor()
-#     cur.execute("""CREATE TABLE IF NOT EXISTS mols
-#             (
-#              id TEXT PRIMARY KEY,
-#              smi TEXT,
-#              smi_protonated TEXT,
-#              source_mol_block TEXT,
-#              source_mol_block_protonated TEXT,
-#              docking_score REAL,
-#              pdb_block TEXT,
-#              mol_block TEXT,
-#              time TEXT
-#             )""")
-#     conn.commit()
-#     data_smi = []  # non 3D structures
-#     data_mol = []  # 3D structures
-#     for mol, mol_name in read_input.read_input(input_fname):
-#         smi = Chem.MolToSmiles(mol, isomericSmiles=True)
-#         if prefix:
-#             mol_name = f'{prefix}-{mol_name}'
-#         if mol_is_3d(mol):
-#             data_mol.append((mol_name, smi, Chem.MolToMolBlock(mol)))
-#         else:
-#             data_smi.append((mol_name, smi))
-#     cur.executemany(f'INSERT INTO mols (id, smi) VALUES(?, ?)', data_smi)
-#     cur.executemany(f'INSERT INTO mols (id, smi, source_mol_block) VALUES(?, ?, ?)', data_mol)
-#     conn.commit()
-#
-#     cur.execute("""CREATE TABLE IF NOT EXISTS setup
-#             (
-#              protonation INTEGER,
-#              protonation_done INTEGER DEFAULT 0,
-#              protein_pdbqt TEXT,
-#              protein_setup TEXT
-#             )""")
-#     conn.commit()
-#     pdbqt_string = open(pdbqt_fname).read()
-#     setup_string = open(protein_setup_fname).read()
-#     cur.execute('INSERT INTO setup VALUES (?,?,?,?)', (int(protonation), 0, pdbqt_string, setup_string))
-#     conn.commit()
-#
-#     conn.close()
+def init_db(db_fname, input_fname, prefix=None):
+
+    conn = sqlite3.connect(db_fname)
+    cur = conn.cursor()
+    data_smi = []  # non 3D structures
+    data_mol = []  # 3D structures
+    for mol, mol_name in read_input.read_input(input_fname):
+        smi = Chem.MolToSmiles(mol, isomericSmiles=True)
+        if prefix:
+            mol_name = f'{prefix}-{mol_name}'
+        if mol_is_3d(mol):
+            data_mol.append((mol_name, smi, Chem.MolToMolBlock(mol)))
+        else:
+            data_smi.append((mol_name, smi))
+    cur.executemany(f'INSERT INTO mols (id, smi) VALUES(?, ?)', data_smi)
+    cur.executemany(f'INSERT INTO mols (id, smi, source_mol_block) VALUES(?, ?, ?)', data_mol)
+    conn.commit()
+
+
+def get_protonation_arg_value(db_fname):
+    """
+    Returns True if molecules had to be protonated and False otherwise
+    :param db_fname:
+    :return:
+    """
+    conn = sqlite3.connect(db_fname)
+    d = yaml.safe_load(conn.execute("SELECT yaml FROM setup").fetchone()[0])
+    return not d['no_protonation']
+
+
+def update_db(db_fname, mol_id, data, table_name='mols'):
+    """
+
+    :param db_fname:
+    :param mol_id: is of a molecule to update values
+    :param data: dict of column names and values to update
+    :param table_name:
+    :return:
+    """
+    conn = sqlite3.connect(db_fname)
+    if data:
+        cols, values = zip(*data.items())
+        conn.execute(f"""UPDATE {table_name}
+                           SET {', '.join(['%s = ?'] * len(cols))},
+                               time = CURRENT_TIMESTAMP
+                           WHERE
+                               id = ?
+                        """ % cols, list(values) + [mol_id])
+        conn.commit()
+
+
+def insert_db(db_fname, data, cols=None, table_name='mols'):
+    """
+
+    :param db_fname:
+    :param data: list of values to insert
+    :param cols: list of corresponding column names in the same order
+    :param table_name:
+    :return:
+    """
+    conn = sqlite3.connect(db_fname)
+    if data:
+        cur = conn.cursor()
+        ncols = len(data)
+        if cols is None:
+            cur.execute(f"INSERT OR IGNORE INTO {table_name} VAlUES({','.join('?' * ncols)})", data)
+        else:
+            cols = ', '.join(cols)
+            cur.execute(f"INSERT OR IGNORE INTO {table_name} ({cols}) VAlUES({','.join('?' * ncols)})", data)
+        conn.commit()
 
 
 def save_sdf(db_fname):
---
+++ moldock/run_dock.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import tempfile
+
+# import dask
+# from dask import bag
+# from dask.distributed import Lock as daskLock, Client
+from moldock.preparation_for_docking import create_db, restore_setup_from_db, init_db, save_sdf, add_protonation, \
+    cpu_type, filepath_type, update_db
+
+
+class RawTextArgumentDefaultsHelpFormatter(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Perform docking of input molecules using Vina 1.2 or Gnina. '
+                                                 'The script automates the whole pipeline: protonates molecules, '
+                                                 'creates 3D structures, converts to PDBQT format, run docking using '
+                                                 'a single machine (multiprocessing) or a cluster of servers (dask), '
+                                                 'stores the best scores and poses in PDBQT and MOL formats to DB.\n'
+                                                 'To run on a single machine:\n'
+                                                 '  run_dock.py -i input.smi -o output.db --program vina --config config.yml -c 4 -v\n\n'
+                                                 'To run on several machines using dask ssh-cluster (on PBS system):\n'
+                                                 '  dask-ssh --hostfile $PBS_NODEFILE --nprocs 32 --nthreads 1 &\n'
+                                                 '  sleep 10\n'
+                                                 '  run_dock.py -i input.smi -o output.db --program vina --config config.yml -c 4 -v --hostfile $PBS_NODEFILE\n\n'
+                                                 '  $PBS_NODEFILE contains the list of addresses of servers\n'
+                                                 'To continue interrupted calculations it is enough to run the script '
+                                                 'with just output argument, all other arguments and data is stored '
+                                                 'in DB. All other arguments passed via command line will be ignored.',
+                                     formatter_class=RawTextArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', metavar='FILENAME', required=False, type=filepath_type,
+                        help='input file with molecules (SMI, SDF, SDF.GZ, PKL). Maybe be omitted if output DB was '
+                             'previosuly created. In this case calculations will be continued from the interrupted '
+                             'point.')
+    parser.add_argument('-o', '--output', metavar='FILENAME', required=True, type=filepath_type,
+                        help='output SQLite DB with scores and poses in PDBQT and MOL formats. It also stores '
+                             'other information (input structures, protein pdbqt file and grid box config). '
+                             'If output DB exists all other inputs will be ignored and calculations will be continued.')
+    parser.add_argument('--program', metavar='STRING', required=False, choices=['vina', 'gnina'],
+                        help='name of a docking program. Choices: vina, gnina')
+    parser.add_argument('--config', metavar='FILENAME', required=False,
+                        help='YAML file with parameters used by docking program.\n'
+                             'vina\n'
+                             'protein: path to pdbqt file with a protein\n'
+                             'protein_setup: path to a text file with coordinates of a binding site\n'
+                             'exhaustiveness: 8\n'
+                             'n_poses: 10\n'
+                             'seed: -1\n'
+                             'gnina\n')
+    parser.add_argument('--no_protonation', action='store_true', default=False,
+                        help='disable protonation of molecules before docking. Protonation requires installed '
+                             'cxcalc chemaxon utility.')
+    parser.add_argument('--sdf', action='store_true', default=False,
+                        help='save best docked poses to SDF file with the same name as output DB.')
+    parser.add_argument('--hostfile', metavar='FILENAME', required=False, type=filepath_type, default=None,
+                        help='text file with addresses of nodes of dask SSH cluster. The most typical, it can be '
+                             'passed as $PBS_NODEFILE variable from inside a PBS script. The first line in this file '
+                             'will be the address of the scheduler running on the standard port 8786. If omitted, '
+                             'calculations will run on a single machine as usual.')
+    parser.add_argument('--tmpdir', metavar='DIRNAME', required=False, type=filepath_type, default=None,
+                        help='path to a dir where to store temporary files accessible to a program. If use dask this '
+                             'argument must be specified because dask cannot access ordinary tmp locations.')
+    parser.add_argument('--prefix', metavar='STRING', required=False, type=str, default=None,
+                        help='prefix which will be added to all molecule names. This might be useful if multiple '
+                             'repeated runs are made which will be analyzed together.')
+    parser.add_argument('-c', '--ncpu', default=1, type=cpu_type,
+                        help='number of cpus.')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='print progress to STDERR.')
+    # parser.add_argument('--table_name', metavar='STRING', required=False, default='mols',
+    #                     help='name of the main table in a database.')
+
+    args = parser.parse_args()
+
+    if args.tmpdir is not None:
+        tempfile.tempdir = args.tmpdir
+
+    tmpfiles = []  # store text files which were saved to the setup table
+
+    try:
+
+        if not os.path.isfile(args.output):
+            create_db(args.output, args)
+            init_db(args.output, args.input, args.prefix)
+        else:
+            args_dict, tmpfiles = restore_setup_from_db(args.output)
+            del args_dict['output']
+            args.__dict__.update(args_dict)
+
+        # if args.hostfile is not None:
+        #     dask.config.set({'distributed.scheduler.allowed-failures': 30})
+        #     dask_client = Client(open(args.hostfile).readline().strip() + ':8786')
+
+        add_protonation(args.output)
+
+        if args.program == 'vina':
+            from moldock.vina_dock import iter_docking
+        elif args.program == 'gnina':
+            from moldock.gnina_dock import iter_docking
+        else:
+            raise ValueError(f'Illegal program argument was supplied: {args.program}')
+
+        i = 0
+        for i, (mol_id, res) in enumerate(iter_docking(args.output, args.config, ncpu=args.ncpu), 1):
+            update_db(args.output, mol_id, res)
+            if args.verbose and i % 100 == 0:
+                sys.stderr.write(f'\r{i} molecules were docked')
+        if args.verbose:
+            sys.stderr.write(f'\n{i} molecules were docked\n')
+
+        if args.sdf:
+            save_sdf(args.output)
+
+    finally:
+
+        for fname in tmpfiles:
+            os.unlink(fname)
+
+
+if __name__ == '__main__':
+    main()
