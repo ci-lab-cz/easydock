0fdc9e6cc1e44ecd673e4644d75b759475d51acb
guzelka <minibaeva07501@gmail.com>
2023-05-11 14:59:30 +0200

    added boron reduction with charge

--- moldock/preparation_for_docking.py
+++ moldock/preparation_for_docking.py
@@ -175,6 +175,8 @@ def ligand_preparation(mol, seed=43):
         if mol:
             idx_boron = [idx for idx, atom in enumerate(mol.GetAtoms()) if atom.GetAtomicNum() == 5]
             for id_ in idx_boron:
+                if mol.GetAtomWithIdx(id_).GetFormalCharge() < 0:
+                    mol.GetAtomWithIdx(id_).SetFormalCharge(0)
                 mol.GetAtomWithIdx(id_).SetAtomicNum(6)
                 # mol.UpdatePropertyCache() # uncomment if necessary
             mol_conf_pdbqt = mk_prepare_ligand(mol, verbose=False)
@@ -220,18 +222,23 @@ def assign_bonds_from_template(template_mol, mol):
 
 
 def boron_reduction(mol_B, mol):
-    idx_boron = [idx for idx, atom in enumerate(mol_B.GetAtoms()) if atom.GetAtomicNum() == 5]
-    for id_ in idx_boron:
+    idx_boron = {idx: atom.GetFormalCharge() for idx, atom in enumerate(mol_B.GetAtoms()) if
+                 atom.GetAtomicNum() == 5}
+    for id_, charge in idx_boron.items():
+        if charge < 0:
+            mol_B.GetAtomWithIdx(id_).SetFormalCharge(0)
         mol_B.GetAtomWithIdx(id_).SetAtomicNum(6)
+
     mol = assign_bonds_from_template(mol_B, mol)
     idx = mol.GetSubstructMatches(mol_B)
-    mol_idx_boron = [tuple(sorted(ids[i] for i in idx_boron)) for ids in idx]
-    mol_idx_boron = list(set(mol_idx_boron)) # retrieve all ids matched possible boron atom positions
-    if len(mol_idx_boron) == 1: # check whether this set of ids is unique
-        for i in mol_idx_boron[0]:
-            mol.GetAtomWithIdx(i).SetAtomicNum(5)
-    else: #if not - several equivalent mappings exist
-        sys.stderr.write('different mappings was detected. The structure cannot be reconstructed automatically.')
+    mol_idx_boron = [(ids[i], j) for i, j in idx_boron.items() for ids in idx]
+    mol_idx_boron = list(set(mol_idx_boron))  # retrieve all ids matched possible boron atom positions
+    if len(mol_idx_boron) == 1:  # check whether this set of ids is unique
+        for id_, charge in mol_idx_boron:
+            mol.GetAtomWithIdx(id_).SetAtomicNum(5)
+            mol.GetAtomWithIdx(id_).SetFormalCharge(charge)
+    else:  # if not - several equivalent mappings exist
+        sys.stderr.write('different mappings was detected. The structure cannot be recostructed automatically.')
         return None
     return mol
 
