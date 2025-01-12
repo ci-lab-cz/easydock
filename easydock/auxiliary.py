3d792926d2efe4b6b26ce0f5a9a3d3dcd1597ed7
Feriolet <veincent.yap@gmail.com>
2025-01-12 13:40:34 +0800

    protonate molecules in batches by looping

--- easydock/auxiliary.py
+++ easydock/auxiliary.py
@@ -1,4 +1,5 @@
 from itertools import islice
+from math import ceil
 import signal
 import functools
 
@@ -57,3 +58,12 @@ def split_generator_to_chunks(generator, chunk_size):
 
     if chunk:
         yield chunk
+
+
+def chunk_into_n(smi_l: list[str], n: int):
+    smi_size = ceil(len(smi_l) / n)
+    return list(map(lambda x: smi_l[x * smi_size:x * smi_size + smi_size], list(range(n))))
+
+
+def chunk_into_size_n(smi_l: list[str], n: int):
+    return [smi_l[i:i+n] for i in range(0,len(smi_l),n)]
